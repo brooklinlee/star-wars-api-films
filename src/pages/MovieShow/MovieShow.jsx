2b02fc6188f90c62b43ff0f62600c3944e362cd0
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-10-10 22:21:52 -0600

    add loading message and basic movie information to MovieShow.jsx

--- src/pages/MovieShow/MovieShow.jsx
+++ src/pages/MovieShow/MovieShow.jsx
@@ -5,21 +5,25 @@ import { useParams } from "react-router-dom"
 // api
 import * as filmsService from '../../../services/sw-api'
 
-const MovieShow = (props) => {
+const MovieShow = () => {
   const [movieDetails, setMovieDetails] = useState({})
   const { movieId } = useParams()
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
-      const movieData = await filmsService.show
+      const movieData = await filmsService.show(movieId)
       setMovieDetails(movieData)
     }
     fetchMovieDetails()
   }, [movieId])
 
+  if (!movieDetails.title) return <h4>Looking in a galaxy far, far away...</h4>
+
   return (  
     <>
-    <h1>Thils is a Movies Details Component</h1>
+    <h1>{movieDetails.title}</h1>
+    <h2>Episode {movieDetails.episode_id}</h2>
+
     </>
   )
 }
