68c0ddc09a870c015cff36fce939781ad080b176
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-10-10 20:34:15 -0600

    stub up nav bar component and landing page component and set routes in app

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+// npm modules
+import { NavLink } from "react-router-dom"
+
+
+
+const NavBar = () => {
+  return (  
+    <nav>
+    <NavLink to="/movies">All Movies</NavLink>
+    </nav>
+
+  )
+}
+
+export default NavBar
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,9 @@
+const Landing = () => {
+  return (  
+    <>
+      <h1>This is a Landing Page Component</h1>
+    </>
+  )
+}
+
+export default Landing
